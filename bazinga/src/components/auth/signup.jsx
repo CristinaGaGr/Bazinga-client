7c75579dcf5147105d744eb0ba0274da970eb54b
Cristina Garcia <cristinagarciagrillo@gmail.com>
2020-04-14 01:51:57 +0200

    logout logic

--- bazinga/src/components/auth/signup.jsx
+++ bazinga/src/components/auth/signup.jsx
@@ -10,7 +10,7 @@ import { useHistory } from 'react-router-dom';
 export const SignUp = () => {
 	const history = useHistory();
 	const [{}, dispatch] = useGlobalContext();
-	const { register, handleSubmit } = useForm();
+	const {register, handleSubmit, errors} = useForm();
 	const onSubmit = data => {
 		const {username, email, password, repeatPassword} = data;
 		console.log(data);
@@ -23,12 +23,38 @@ export const SignUp = () => {
 	};
 
 	return (
-		<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-			<input type="text" placeholder="Username" name="username" ref={register({required: true})} />
-			<input type="email" placeholder="Email" name="email" ref={register} />
-			<input type="password" placeholder="Password" name="password" ref={register({required: true, min: 6})} />
-			<input type="password" placeholder="Repeat Password" name="repeatPassword" ref={register} />
-
+		<form noValidate={true} className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+			<div className={styles.inputContainer}>
+				<input type="text" placeholder="Username" name="username" ref={register({required: true})}/>
+				{errors.username && <span
+					className={styles.errorMessage}>{errors.username.message ? errors.username.message : 'this field is required'}</span>}
+			</div>
+			<div className={styles.inputContainer}>
+				<input type="email" placeholder="Email" name="email" ref={register({
+					required: true,
+					pattern: {
+						value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+						message: 'invalid email address'
+					}
+				})}/>
+				{errors.email && <span
+					className={styles.errorMessage}>{errors.email.message ? errors.email.message : 'this field is required'}</span>}
+			</div>
+			<div className={styles.inputContainer}>
+				<input type="password" placeholder="Password" name="password"
+					   ref={register({required: true, minLength: {
+					   			value: 6,
+							   message: 'min 6 characters'
+						   }})}/>
+				{errors.password && <span
+					className={styles.errorMessage}>{errors.password.message ? errors.password.message : 'this field is required'}</span>}
+			</div>
+			<div className={styles.inputContainer}>
+				<input type="password" placeholder="Repeat Password" name="repeatPassword"
+					   ref={register({required: true})}/>
+				{errors.repeatPassword && <span
+					className={styles.errorMessage}>{errors.repeatPassword.message ? errors.repeatPassword.message : 'this field is required'}</span>}
+			</div>
 			<button className={'btn'} type={'submit'}>Sign Up</button>
 		</form>
 	)
--- bazinga/src/components/history/history.jsx
+++ bazinga/src/components/history/history.jsx
@@ -2,20 +2,34 @@ import React, { useEffect, useState } from 'react';
 import './history.scss';
 import { HistoryCard } from './history-card/history-card';
 import { getSummary } from '../../api/core.api';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { logout } from '../../api/auth.api';
+import { setUserAction } from '../../context/actions';
+import { useGlobalContext } from '../../context/context';
+
 
 export const History = () => {
-	const [history, setHistory] = useState([]);
+	const history = useHistory();
+	const [summary, setSummary] = useState([]);
+	const [{}, dispatch] = useGlobalContext();
+
 
 	useEffect(() => {
 		// getSummary().then((res) => setHistory(res));
 	},[]);
 
+	const handleLogout = () => {
+			logout().then(() => {
+				dispatch(setUserAction(null));
+				history.push('/');
+			})
+	};
+
 	return (
 		<div>
-			<Link to={'/'}>Log Out</Link>
+			<button className={'btn'} onClick={handleLogout} >Log Out</button>
 			<h1>History</h1>
-			{history.map((e) =>
+			{summary.map((e) =>
 				<HistoryCard/>
 			)}
 		</div>
